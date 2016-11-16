155f8a7186bb7259560a1e83f9b8b785c5821a95
Yuri Reyes <reyestomas13@gmail.com>
2016-11-15 19:18:09 -0700

    Mentor Availability complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -19,4 +19,10 @@ class Kele
 		response = self.class.get '/users/me', headers: {'authorization' => @auth_token}
 		JSON.parse(response.body)
 	end
+
+  def get_mentor_availability mentor_id
+		response = self.class.get "/mentors/#{mentor_id}/student_availability", headers: {'authorization' => @auth_token}
+		JSON.parse(response.body)
+	end
+  
 end
