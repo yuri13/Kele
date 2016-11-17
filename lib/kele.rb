467b89293b91b33351b645fd2fc575442ceccd7d
Yuri Reyes <reyestomas13@gmail.com>
2016-11-17 15:23:07 -0700

    Messaging complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -27,4 +27,44 @@ class Kele
 		JSON.parse(response.body)
 	end
 
+	def get_messages page_id = -1
+		if page_id == -1
+			page_id = 1
+			response = get_response page_id
+			response_array = []
+			while !response.parsed_response["items"].empty?
+				response_array << JSON.parse(response.body)
+				page_id += 1
+				response = get_response page_id
+			end
+			response_array
+		else
+			response = get_response page_id
+			JSON.parse(response.body)
+		end
+	end
+
+	def create_message sender, recipient_id, subject, stripped_text, token = ""
+			body = {
+							'sender': sender.to_s,
+							'recipient_id': recipient_id.to_s,
+							'subject': subject,
+							'stripped_text': stripped_text
+			}
+
+			body['token'] = token unless token.empty?
+
+			response = self.class.post "/messages", headers: {'authorization' => @auth_token}, body: body
+			begin
+							JSON.parse(response.body)
+			rescue	JSON::ParserError
+							response.body
+			end
+	end
+
+	private
+	def get_response page_id
+		self.class.get "/message_threads", headers: {'authorization' => @auth_token}, body: {'page' => page_id.to_s}
+	end
+
 end
